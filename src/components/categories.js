089784a1a59f6ed6685faa80593595ab727fef83
Darren Hoyland <darren@hoyland.me>
2020-03-28 19:08:03 +0000

    tags and categories (2nd level link still doesn't work - also, I need paginationa nd/or a tag cloud

--- src/components/categories.js
+++ src/components/categories.js
@@ -1,12 +1,72 @@
 import React from "react"
+// import PropTypes from "prop-types"
+
+// Utilities
+import kebabCase from "lodash/kebabCase"
+
+// Components
+import { Link, StaticQuery, graphql } from "gatsby"
 
 
-const Categories = () => {
-  return (
-    <div>
-        Categories
+const Categories = () => (
+
+  <StaticQuery
+    query={graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+        allMarkdownRemark(limit: 2000) {
+          group(field: frontmatter___categories) {
+            fieldValue
+            totalCount
+          }
+        }
+      }
+    `}
+    render={data => (
+      <div>
+        <div>
+          <h1>Categories</h1>
+          <ul>
+            {data.allMarkdownRemark.group.map(category => (
+              <li key={category.fieldValue}>
+                <Link to={`/categories/${kebabCase(category.fieldValue)}/`}>
+                  {category.fieldValue} ({category.totalCount})
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
     </div>
-  )
-}
+    )}
+  />
+)
+
 
 export default Categories
+
+
+
+// combined:
+// `
+//   query {
+//     site {
+//       siteMetadata {
+//         title
+//       }
+//     }
+//     allMarkdownRemark(limit: 2000){
+//       categories: group(field: frontmatter___categories) {
+//         fieldValue
+//         totalCount
+//       }
+//       categories: group(field: frontmatter___categories) {
+//         fieldValue
+//         totalCount
+//       }
+//     }
+//   }
+// `
--- src/components/tags.js
+++ src/components/tags.js
@@ -1,83 +1,72 @@
 import React from "react"
-import PropTypes from "prop-types"
+// import PropTypes from "prop-types"
 
 // Utilities
 import kebabCase from "lodash/kebabCase"
 
 // Components
-import { Link, graphql } from "gatsby"
+import { Link, StaticQuery, graphql } from "gatsby"
 
 
-// const Tags = ({
-//   data: {
-//     allMarkdownRemark: { group },
-//     site: {
-//       siteMetadata: { title },
-//     },
-//   },
-// }) => {
-//   return (
-//     <div>
-//         hi
-//     </div>
-//   )
-// }
+const Tags = () => (
 
-// export default Tags
-
-
-
-
-const Tags = (data) => (
- <div>
-    <div>
-      <h1>Tags</h1>
-      <ul>
-        {/* {group.map(tag => (
-          <li key={tag.fieldValue}>
-            <Link to={`/tags/${kebabCase(tag.fieldValue)}/`}>
-              {tag.fieldValue} ({tag.totalCount})
-            </Link>
-          </li>
-        ))} */}
-      </ul>
+  <StaticQuery
+    query={graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+        allMarkdownRemark(limit: 2000) {
+          group(field: frontmatter___tags) {
+            fieldValue
+            totalCount
+          }
+        }
+      }
+    `}
+    render={data => (
+      <div>
+        <div>
+          <h1>Tags</h1>
+          <ul>
+            {data.allMarkdownRemark.group.map(tag => (
+              <li key={tag.fieldValue}>
+                <Link to={`/tags/${kebabCase(tag.fieldValue)}/`}>
+                  {tag.fieldValue} ({tag.totalCount})
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
     </div>
- </div>
+    )}
+  />
 )
 
-Tags.propTypes = {
-  data: PropTypes.shape({
-    allMarkdownRemark: PropTypes.shape({
-      group: PropTypes.arrayOf(
-        PropTypes.shape({
-          fieldValue: PropTypes.string.isRequired,
-          totalCount: PropTypes.number.isRequired,
-        }).isRequired
-      ),
-    }),
-    site: PropTypes.shape({
-      siteMetadata: PropTypes.shape({
-        title: PropTypes.string.isRequired,
-      }),
-    }),
-  }),
-}
 
 export default Tags
 
-export const pageQuery = graphql`
-  query {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-    allMarkdownRemark(limit: 2000) {
-      group(field: frontmatter___tags) {
-        fieldValue
-        totalCount
-      }
-    }
-  }
-`
 
+
+// combined:
+// `
+//   query {
+//     site {
+//       siteMetadata {
+//         title
+//       }
+//     }
+//     allMarkdownRemark(limit: 2000){
+//       tags: group(field: frontmatter___tags) {
+//         fieldValue
+//         totalCount
+//       }
+//       categories: group(field: frontmatter___categories) {
+//         fieldValue
+//         totalCount
+//       }
+//     }
+//   }
+// `
